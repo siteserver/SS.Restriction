5c22f579b282bb966706a34286080122a82e4aec
starlying <corp@siteserver.cn>
2018-01-20 06:46:06 +0800

    assembly

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("SS.Restriction")]
+[assembly: AssemblyDescription("SiteServer CMS Restriction plugin.")]
 [assembly: AssemblyCopyright("Copyright © SiteServer CMS 2018")]
+[assembly: Guid("4b56a878-e06a-42ce-8b29-b2bcf7868668")]
